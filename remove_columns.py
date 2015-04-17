e8f3dcb3bc1a15a61eb0abb530553cb1b0ea12bd
lukas <lhofmaie@hsr.ch>
2015-04-17 18:57:17 +0200

    argparse in remove column hinzugefuegt

--- remove_columns.py
+++ remove_columns.py
@@ -1,8 +1,18 @@
+import argparse
+
+line1 = "1	1	5	874965758"
+line2 = "1	2	3	876893171"
+lines = [line1, line2]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("task", help="set task remove_colum, tosparse")
+args = parser.parse_args()
+if args.task is "remove_c":
 f = open('/home/lukas/Downloads/ml-100k/u1.base', 'r')
 output = open('/home/lukas/Documents/infile','w')
 lines = list(f)
 writelines = []
-for line in lines[0:4]:
+for line in lines:
     words = line.split()
     writelines.append("\t".join(words[0:2]))
 
