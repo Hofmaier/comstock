8fa14b4139905ffe6db5e52df3c37573b70ce3d2
lukas <lhofmaie@hsr.ch>
2015-04-17 16:04:24 +0200

    skript fuer spark-itemsimilarity erstellt

--- run_spark_itemsimilarity.sh
+++ run_spark_itemsimilarity.sh
@@ -1,3 +1,5 @@
 export MAHOUT_HOME=~/Downloads/mahout-distribution-0.10.0/
 export SPARK_HOME=~/Downloads/spark-1.3.0/
 export MASTER=spark://case:7077
+
+/home/lukas/Downloads/mahout-distribution-0.10.0/bin/mahout spark-itemsimilarity --input $infile --output $outfile
