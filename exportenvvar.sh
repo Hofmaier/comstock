b257bf2e9ac5763cac540a665ebdc31f1a3d8999
lukas <lhofmaie@hsr.ch>
2015-05-06 15:21:10 +0200

    example input file erstellt

---
+++ exportenvvar.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+export MAHOUT_HOME=~/Downloads/mahout-distribution-0.10.0/
+export SPARK_HOME=~/Downloads/spark-1.3.0/
+export MASTER=spark://case:7077
+
+/home/lukas/Downloads/mahout-distribution-0.10.0/bin/mahout spark-itemsimilarity
