cec93e15121983a97f35bdfa8c1effa59500e24b
lukas <lhofmaie@hsr.ch>
2015-06-28 15:05:14 +0200

    spark script an neue version angepasst

--- run_spark_itemsimilarity.sh
+++ run_spark_itemsimilarity.sh
@@ -1,9 +1,9 @@
 export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
-export MAHOUT_HOME=~/Downloads/mahout-distribution-0.10.0/
-export SPARK_HOME=~/Downloads/spark-1.3.0/
+export MAHOUT_HOME=~/Downloads/mahout-distribution-0.10.1/
+export SPARK_HOME=~/Downloads/spark-1.4.0/
 export MASTER=spark://case:7077
 
 infile="/home/lukas/Documents/infile"
 outfile="/home/lukas/Documents/outfile/"
 
-/home/lukas/Downloads/mahout-distribution-0.10.0/bin/mahout spark-itemsimilarity --input $infile --output $outfile
+/home/lukas/Downloads/apache-mahout-distribution-0.10.1/bin/mahout spark-itemsimilarity --input $infile --output $outfile
