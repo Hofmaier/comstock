e846df7fa5d6b61127f1e8fb31f86d6b81f2cad0
lukas <lhofmaie@hsr.ch>
2015-06-28 10:17:23 +0200

    windows scripts verschoben

--- setjavaHome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-cd ~/Downloads/spark-1.3.0
-mvn -DskipTests clean package
--- setpath.ps1
+++
@@ -1,1 +0,0 @@
-﻿setx PATH "$env:path;C:\Program Files\apache-maven-3.3.1-bin\apache-maven-3.3.1\bin\" /M
