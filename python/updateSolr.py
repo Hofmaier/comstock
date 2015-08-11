c50c468bb559e7050ad8d9b113b70275a3931931
lukas <lhofmaie@hsr.ch>
2015-08-11 14:51:17 +0200

    line2indicators erstellt

--- python/updateSolr.py
+++ python/updateSolr.py
@@ -4,6 +4,7 @@ import os
 import pysolr
 import json
 import argparse
+import re
 
 parser = argparse.ArgumentParser(
     description='Solr update utility',
@@ -24,10 +25,26 @@ args = parser.parse_args()
 dbfilepath = args.db
 solrinstance = args.solr
 rawdatafilepath = './inputfile'
-sparkoutputdir = '/home/lukas/Documents/outfile'
+sparkoutputdir = './outfile'
 mahouthome = os.getenv('MAHOUT_HOME')
 print mahouthome
-mahoutshellpath = '/bin/mahout spark-itemsimilarity'
+mahoutshellpath = mahouthome + 'bin/mahout spark-itemsimilarity'
+
+def line2indicators(line):
+    idsims = line.split('\t')
+    if len(idsims) > 1:
+        mid = idsims[0]
+        similarities = idsims[1]
+        p = re.compile('\d*:\d*.\d*')
+        allsims = p.findall(line)
+        indicators = ''
+        for idsim in allsims:
+            idsiml = idsim.split(':')
+            mid = idsiml[0]
+            payloadstr = idsiml[0] + '|' + idsiml[1] + ' '
+            indicators = indicators + payloadstr
+        return (idsims[0],indicators)
+    return (idsims[0].strip(),'')
 
 #'/home/lukas/Downloads/apache-mahout-distribution-0.10.1/bin/mahout spark-itemsimilarity'
 
@@ -41,7 +58,7 @@ with open(rawdatafilepath, 'w') as rawdatafile:
         csvwriter.writerow([r[0],r[1],'like'])
 
 #os.putenv('JAVA_HOME','/usr/lib/jvm/java-1.8.0-openjdk-amd64')
-#os.putenv('MAHOUT_HOME','/home/lukas/Downloads/mahout-distribution-0.10.1/')
+#os.putenv('MAHOUT_HOME','/home/lukas/Downloads/apache-mahout-distribution-0.10.1/')
 #os.putenv('SPARK_HOME','/home/lukas/Downloads/spark-1.3.1/')
 #os.putenv('MASTER','spark://case:7077')
 
@@ -49,7 +66,7 @@ executellrcmd = mahoutshellpath + ' --input ' + rawdatafilepath + ' --output ' +
 #os.system(executellrcmd)
 
 solr = pysolr.Solr(solrinstance)
-sparkoutputfile = sparkoutputdir + '/part0'
+sparkoutputfile = sparkoutputdir + '/similarity-matrix/part-00000'
 solrdocs = []
 movies = {}
 cursor.execute('''SELECT * FROM movie''')
@@ -63,13 +80,9 @@ for m in cursor:
 with open(sparkoutputfile, 'r') as similarityfile:
     lines = list(similarityfile)
     for line in lines:
-        idsim = line.split('\\')
-        mid = idsim[0]
-        otheridsim = idsim[1].split(':')
-        payloadstr = otheridsim[0] + '|' + otheridsim[1] + ' '
-        if mid in movies:
-            m = movies[mid]
-            m['payloads'] = m['payloads'] + payloadstr
+        midindicator = line2indicators(line)
+        movie = movies[midindicator[0]]
+        movie['payloads'] = midindicator[1]
       
 solr.delete(q='*:*')
 l = [movie for k, movie in movies.iteritems()]
