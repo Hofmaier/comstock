fdc537dfc66b3d3ca6e1f7d9fa1757193f9eea15
lukas <lhofmaie@hsr.ch>
2015-08-11 10:27:45 +0200

    add shellscripts

---
+++ shellscripts/run_spark_itemsimilarity.sh
@@ -0,0 +1,11 @@
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+export MAHOUT_HOME=~/Downloads/mahout-distribution-0.10.1/
+export SPARK_HOME=~/Downloads/spark-1.3.1/
+export MASTER=spark://case:7077
+
+infile="/home/lukas/Documents/infile/inputfile"
+outfile="/home/lukas/Documents/outfile/"
+
+/home/lukas/Downloads/mahout-distribution-0.10.0/bin/mahout spark-itemsimilarity --input $infile --output $outfile
+
+#spark-itemsimilarity --input $infile --output $outfile
