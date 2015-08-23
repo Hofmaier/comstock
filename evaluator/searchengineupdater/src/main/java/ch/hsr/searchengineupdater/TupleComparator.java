2fb1cefa5fa608beb7789e46377184778705b348
lukas <lhofmaie@hsr.ch>
2015-08-23 13:38:40 +0200

    pom aktualisiert

--- evaluator/searchengineupdater/src/main/java/ch/hsr/searchengineupdater/TupleComparator.java
+++ evaluator/searchengineupdater/src/main/java/ch/hsr/searchengineupdater/TupleComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class TupleComparator implements Comparator<Tuple> {
 
-	@Override
+	
 	public int compare(Tuple arg0, Tuple arg1) {
 		return Double.compare(arg0.similarity , arg1.similarity)  * -1;
 	}
